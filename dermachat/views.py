89130acad95db658cb2c5c7181ad55e70a80d86a
Francesco <francesco.daleo@bts.tech>
2024-04-22 09:27:35 +0200

    add_handling_exeption_botocore

--- dermachat/views.py
+++ dermachat/views.py
@@ -1,13 +1,3 @@
-# from pipino_doctorino.settings import Credentials
-
-# credentials = Credentials()
-# AWS_ACCESS_KEY_ID = credentials.AWS_ACCESS_KEY_ID
-# AWS_SECRET_ACCESS_KEY = credentials.AWS_SECRET_ACCESS_KEY
-# AWS_SECRET_TOKEN = credentials.AWS_SECRET_TOKEN
-# AWS_STORAGE_BUCKET_NAME = credentials.AWS_STORAGE_BUCKET_NAME
-# AWS_STORAGE_PATH = credentials.AWS_STORAGE_PATH
-# AWS_PICKLE_MODEL = credentials.AWS_PICKLE_MODEL
-
 from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import ImageUploadForm
@@ -19,10 +9,9 @@ import timm
 import boto3
 import os
 import datetime
+import botocore
 
 from pipino_doctorino.settings import Credentials
-
-s3 = boto3.client('s3')
 
 # Assuming credentials contains AWS credentials
 credentials = Credentials()
@@ -38,37 +27,56 @@ if not os.path.exists(local_model_dir):
 # Local path to save the model
 local_model_path = os.path.join(local_model_dir, os.path.basename(model_path))
 
-# Initialize S3 client
-s3 = boto3.client('s3')
+try:
+    # Initialize S3 client
+    s3 = boto3.client('s3')
 
-# Check if the local model file exists
-if os.path.exists(local_model_path):
-    # Get last modified date of local model file
-    local_last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(local_model_path))
-    
-    # Get last modified date of model file on S3
-    response = s3.head_object(Bucket=bucket_name, Key=model_path)
-    s3_last_modified = response['LastModified'].replace(tzinfo=None)
+    # Check if the local model file exists
+    if os.path.exists(local_model_path):
+        # Get last modified date of local model file
+        local_last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(local_model_path))
 
-    # Compare last modified dates
-    if s3_last_modified > local_last_modified:
-        # Download the newer version from S3
+        # Get last modified date of model file on S3
+        response = s3.head_object(Bucket=bucket_name, Key=model_path)
+        s3_last_modified = response['LastModified'].replace(tzinfo=None)
+
+        # Compare last modified dates
+        if s3_last_modified > local_last_modified:
+            # Download the newer version from S3
+            s3.download_file(bucket_name, model_path, local_model_path)
+    else:
+        # If the local file doesn't exist, download it from S3
         s3.download_file(bucket_name, model_path, local_model_path)
+
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == 'NoSuchKey':
+        print("S3 not found")
+    else:
+        print("An error occurred with botocore:", e)
+    # Use the model in the models folder locally
+
+    # Initialize Xception model
+    model = timm.create_model('legacy_xception', pretrained=False)
+    num_ftrs = model.fc.in_features
+    model.fc = torch.nn.Linear(num_ftrs, 2)  # Modify fully connected layer for your task
+
+    # Load model state dictionary from the locally downloaded file
+    checkpoint = torch.load(local_model_path)
+    model.load_state_dict(checkpoint)
+
 else:
-    # If the local file doesn't exist, download it from S3
-    s3.download_file(bucket_name, model_path, local_model_path)
+    # Initialize Xception model
+    model = timm.create_model('legacy_xception', pretrained=False)
+    num_ftrs = model.fc.in_features
+    model.fc = torch.nn.Linear(num_ftrs, 2)  # Modify fully connected layer for your task
 
-# Initialize Xception model
-model = timm.create_model('legacy_xception', pretrained=False)
-num_ftrs = model.fc.in_features
-model.fc = torch.nn.Linear(num_ftrs, 2)  # Modify fully connected layer for your task
-
-# Load model state dictionary from the locally downloaded file
-checkpoint = torch.load(local_model_path)
-model.load_state_dict(checkpoint)
+    # Load model state dictionary from the locally downloaded file
+    checkpoint = torch.load(local_model_path)
+    model.load_state_dict(checkpoint)
 
 # Set model to evaluation mode
 model.eval()
+
 
 # Function to preprocess the image
 def preprocess_image(image):
@@ -79,13 +87,12 @@ def preprocess_image(image):
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     image = transform(image)
-    return image.unsqueeze(0)  # Add batch dimension
+    return image.unsqueeze(0)  
 
 def upload_image(request):
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            # Read the uploaded image
             image_file = request.FILES['image']
             image_data = np.frombuffer(image_file.read(), np.uint8)
             image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
