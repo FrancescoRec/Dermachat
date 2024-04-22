b69d7ad88d8a1d03f21499b1ba29c48813696a42
Francesco <francesco.daleo@bts.tech>
2024-04-22 14:00:21 +0200

    handling

--- dermachat/views.py
+++ dermachat/views.py
@@ -6,17 +6,20 @@ import numpy as np
 import torch
 import torchvision.transforms as transforms
 import timm
-import boto3
 import os
 import datetime
-import botocore
+
 
 from pipino_doctorino.settings import Credentials
 
 # Assuming credentials contains AWS credentials
-credentials = Credentials()
-model_path = credentials.AWS_STORAGE_PATH
-bucket_name = credentials.AWS_STORAGE_BUCKET_NAME
+try:
+    credentials = Credentials()
+    model_path = credentials.AWS_STORAGE_PATH
+    bucket_name = credentials.AWS_STORAGE_BUCKET_NAME
+except AttributeError:
+    model_path = 'models/cnn/Xception_model.pth'  
+    bucket_name = None
 
 local_model_dir = 'models/cnn'
 
@@ -27,60 +30,53 @@ if not os.path.exists(local_model_dir):
 # Local path to save the model
 local_model_path = os.path.join(local_model_dir, os.path.basename(model_path))
 
-try:
-    # Initialize S3 client
-    s3 = boto3.client('s3')
+if bucket_name:
+    try:
+        import botocore
+        import boto3
+        # Initialize S3 client
+        s3 = boto3.client('s3')
 
-    # Check if the local model file exists
-    if os.path.exists(local_model_path):
-        # Get last modified date of local model file
-        local_last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(local_model_path))
+        # Check if the local model file exists
+        if os.path.exists(local_model_path):
+            # Get last modified date of local model file
+            local_last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(local_model_path))
 
-        try:
-            # Get last modified date of model file on S3
-            response = s3.head_object(Bucket=bucket_name, Key=model_path)
-            s3_last_modified = response['LastModified'].replace(tzinfo=None)
+            try:
+                # Get last modified date of model file on S3
+                response = s3.head_object(Bucket=bucket_name, Key=model_path)
+                s3_last_modified = response['LastModified'].replace(tzinfo=None)
 
-            # Compare last modified dates
-            if s3_last_modified > local_last_modified:
-                # Download the newer version from S3
-                s3.download_file(bucket_name, model_path, local_model_path)
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] == 'NoSuchKey':
-                print("S3 not found")
-            else:
-                print("An error occurred with botocore:", e)
-                # Fallback to using the model in the models folder locally
-                raise e
-    else:
-        # If the local file doesn't exist, download it from S3
-        s3.download_file(bucket_name, model_path, local_model_path)
+                # Compare last modified dates
+                if s3_last_modified > local_last_modified:
+                    # Download the newer version from S3
+                    s3.download_file(bucket_name, model_path, local_model_path)
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == 'NoSuchKey':
+                    print("S3 not found")
+                else:
+                    print("An error occurred with botocore:", e)
+                    # Fallback to using the model in the models folder locally
+                    raise e
+        else:
+            # If the local file doesn't exist, download it from S3
+            s3.download_file(bucket_name, model_path, local_model_path)
 
-except botocore.exceptions.NoCredentialsError:
-    print("AWS credentials not found. Falling back to local resources.")
+    except (ImportError, botocore.exceptions.NoCredentialsError, botocore.exceptions.ClientError) as e:
+        print("AWS credentials not found or error occurred. Falling back to local resources.")
+        bucket_name = None
 
-    # Initialize Xception model
-    model = timm.create_model('legacy_xception', pretrained=False)
-    num_ftrs = model.fc.in_features
-    model.fc = torch.nn.Linear(num_ftrs, 2)  # Modify fully connected layer for your task
+# Initialize Xception model
+model = timm.create_model('legacy_xception', pretrained=False)
+num_ftrs = model.fc.in_features
+model.fc = torch.nn.Linear(num_ftrs, 2)  # Modify fully connected layer for your task
 
-    # Load model state dictionary from the locally downloaded file
-    checkpoint = torch.load(local_model_path)
-    model.load_state_dict(checkpoint)
-
-else:
-    # Initialize Xception model
-    model = timm.create_model('legacy_xception', pretrained=False)
-    num_ftrs = model.fc.in_features
-    model.fc = torch.nn.Linear(num_ftrs, 2)  # Modify fully connected layer for your task
-
-    # Load model state dictionary from the locally downloaded file
-    checkpoint = torch.load(local_model_path)
-    model.load_state_dict(checkpoint)
+# Load model state dictionary from the locally downloaded file
+checkpoint = torch.load(local_model_path)
+model.load_state_dict(checkpoint)
 
 # Set model to evaluation mode
 model.eval()
-
 
 # Function to preprocess the image
 def preprocess_image(image):
