a03ea6c9a5cad7fe0dbf0d15a9d368f525ffb193
Francesco <francesco.daleo@bts.tech>
2024-04-29 14:57:52 +0200

    little_change_for_cleaning_and_adapt

--- dermachat/views.py
+++ dermachat/views.py
@@ -7,69 +7,15 @@ import torch
 import torchvision.transforms as transforms
 import timm
 import os
-import datetime
 
 
-from pipino_doctorino.settings import Credentials
-
-# Assuming credentials contains AWS credentials
-try:
-    credentials = Credentials()
-    model_path = credentials.AWS_STORAGE_PATH
-    bucket_name = credentials.AWS_STORAGE_BUCKET_NAME
-except AttributeError:
-    model_path = 'models/cnn/Xception_model.pth'  
-    bucket_name = None
-
-local_model_dir = 'models/cnn'
-
-# Create the local directory if it doesn't exist
-if not os.path.exists(local_model_dir):
-    os.makedirs(local_model_dir)
-
-# Local path to save the model
-local_model_path = os.path.join(local_model_dir, os.path.basename(model_path))
-
-if bucket_name:
-    try:
-        import botocore
-        import boto3
-        # Initialize S3 client
-        s3 = boto3.client('s3')
-
-        # Check if the local model file exists
-        if os.path.exists(local_model_path):
-            # Get last modified date of local model file
-            local_last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(local_model_path))
-
-            try:
-                # Get last modified date of model file on S3
-                response = s3.head_object(Bucket=bucket_name, Key=model_path)
-                s3_last_modified = response['LastModified'].replace(tzinfo=None)
-
-                # Compare last modified dates
-                if s3_last_modified > local_last_modified:
-                    # Download the newer version from S3
-                    s3.download_file(bucket_name, model_path, local_model_path)
-            except botocore.exceptions.ClientError as e:
-                if e.response['Error']['Code'] == 'NoSuchKey':
-                    print("S3 not found")
-                else:
-                    print("An error occurred with botocore:", e)
-                    # Fallback to using the model in the models folder locally
-                    raise e
-        else:
-            # If the local file doesn't exist, download it from S3
-            s3.download_file(bucket_name, model_path, local_model_path)
-
-    except (ImportError, botocore.exceptions.NoCredentialsError, botocore.exceptions.ClientError) as e:
-        print("AWS credentials not found or error occurred. Falling back to local resources.")
-        bucket_name = None
+model_name = 'Xception'
+local_model_path = os.path.join('models', 'cnn', f'{model_name}_model.pth')
 
 # Initialize Xception model
 model = timm.create_model('legacy_xception', pretrained=False)
 num_ftrs = model.fc.in_features
-model.fc = torch.nn.Linear(num_ftrs, 2)  # Modify fully connected layer for your task
+model.fc = torch.nn.Linear(num_ftrs, 2)  
 
 # Load model state dictionary from the locally downloaded file
 checkpoint = torch.load(local_model_path)
